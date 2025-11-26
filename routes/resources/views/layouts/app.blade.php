004618f6da6e8d0903c5008b97976a4be4bbbc50
jhair anderson <jhairandersonaguilarpalacios37@gmail.com>
2025-11-26 09:17:15 -0400

    Agregar panel de usuario en sidebar con nombre del operador autenticado

--- routes/resources/views/layouts/app.blade.php
+++ routes/resources/views/layouts/app.blade.php
@@ -123,6 +123,23 @@
         <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Trazabilidad</span>
     </a>
+    
+    <!-- Sidebar user panel -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img src="https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">
+                @auth
+                    {{ Auth::user()->first_name }} {{ Auth::user()->last_name }}
+                @else
+                    Usuario
+                @endauth
+            </a>
+        </div>
+    </div>
+    
     <div class="sidebar">
 
         <nav class="mt-2">
